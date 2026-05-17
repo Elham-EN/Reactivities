0396cdc0b8c7c7d4c963ab928507cd89407c8120
Elham-EN <ilhamrezaie@gmail.com>
2026-05-17 14:19:16 +1000

    created entity class for Acitvity in Domain Project

---
+++ Domain/Activity.cs
@@ -0,0 +1,21 @@
+namespace Domain
+{
+    // Entity class relate to a table in the database
+    public class Activity
+    {
+        // Default value initializer, so every time you do new Activity() it 
+        // automatically generates a unique ID. You never have to set it manually.
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string Title { get; set; }
+        public DateTime Date { get; set; }
+        public required string Description { get; set; }
+        public required string Category { get; set; }
+        public bool IsCancelled { get; set; }
+
+        // Location props
+        public required string City { get; set; }
+        public required string Venue { get; set; }
+        public required double Latitude { get; set; }
+        public required double Longitude { get; set; }
+    }
+}
