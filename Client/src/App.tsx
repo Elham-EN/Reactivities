0a88df58308c2ad9b7b519dc1a508dfaa91e3686
Elham-EN <ilhamrezaie@gmail.com>
2026-05-18 23:09:07 +1000

    use axios with typescript type support

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import type { Activitiy } from "./lib/types/index.type";
+import axios from "axios";
 import { List, ListItem, ListItemText, Typography } from "@mui/material";
 
 function App(): React.ReactElement {
   const [activities, setActivities] = React.useState<Activitiy[]>([]);
 
   React.useEffect(() => {
-    fetch("https://localhost:5001/api/Activities").then((response) =>
-      response.json().then((data) => setActivities(data)),
-    );
+    try {
+      axios
+        .get<Activitiy[]>("https://localhost:5001/api/Activities")
+        .then((response) => setActivities(response.data));
+    } catch (error) {
+      console.error(error);
+    }
   }, []);
 
   return (
