3838158268a8f1336ee9860a192aca00d8a7bac5
Elham-EN <ilhamrezaie@gmail.com>
2026-05-25 17:29:49 +1000

    integrated Tanstack React Query & replaced useEffect

--- Client/src/app/layout/App.tsx
+++ Client/src/app/layout/App.tsx
@@ -1,29 +1,29 @@
 import React from "react";
 import type { Activitiy } from "../../lib/types/index.type";
 import axios from "axios";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/ActivityDashboard";
+import { useQuery } from "@tanstack/react-query";
 
 function App(): React.ReactElement {
-  const [activities, setActivities] = React.useState<Activitiy[]>([]);
   const [selectedActivity, setSelectedActivity] = React.useState<
     Activitiy | undefined
   >(undefined);
   const [editMode, setEditMode] = React.useState<boolean>(false);
 
-  React.useEffect(() => {
-    try {
-      axios
-        .get<Activitiy[]>("https://localhost:5001/api/Activities")
-        .then((response) => setActivities(response.data));
-    } catch (error) {
-      console.error(error);
-    }
-  }, []);
+  const { data: activities, isPending } = useQuery({
+    queryKey: ["activities"],
+    queryFn: async () => {
+      const response = await axios.get<Activitiy[]>(
+        "https://localhost:5001/api/Activities",
+      );
+      return response.data;
+    },
+  });
 
   const handleSelectActivity = (id: string): void => {
-    setSelectedActivity(activities.find((x) => x.id === id));
+    setSelectedActivity(activities!.find((x) => x.id === id));
   };
 
   const handleCancelSelectActivity = (): void => {
@@ -41,43 +41,46 @@ function App(): React.ReactElement {
   };
 
   const handleSumbitForm = (activity: Activitiy): void => {
-    // Edit / Update activity
-    if (activity.id) {
-      // Either set state to updated activity if id exits otherwise
-      // the original activity
-      setActivities(
-        activities.map((x) => (x.id === activity.id ? activity : x)),
-      );
-      // Create new activity
-    } else {
-      const newActivity = { ...activity, id: activities.length.toString() };
-      setSelectedActivity(newActivity);
-      // Add new activity
-      setActivities([...activities, newActivity]);
-    }
-    //
+    // // Edit / Update activity
+    // if (activity.id) {
+    //   // Either set state to updated activity if id exits otherwise
+    //   // the original activity
+    //   setActivities(
+    //     activities.map((x) => (x.id === activity.id ? activity : x)),
+    //   );
+    //   // Create new activity
+    // } else {
+    //   const newActivity = { ...activity, id: activities.length.toString() };
+    //   setSelectedActivity(newActivity);
+    //   // Add new activity
+    //   setActivities([...activities, newActivity]);
+    // }
     setEditMode(false);
   };
 
   const handleDelete = (id: string): void => {
-    setActivities(activities.filter((x) => x.id !== id));
+    // setActivities(activities.filter((x) => x.id !== id));
   };
 
   return (
     <>
       <NavBar openForm={handleOpenForm} />
       <Container maxWidth="xl" sx={{ mt: 3 }}>
-        <ActivityDashboard
-          activities={activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          editMode={editMode}
-          openForm={handleOpenForm}
-          closeForm={handleFormClose}
-          submitForm={handleSumbitForm}
-          deleteActivity={handleDelete}
-        />
+        {!activities || isPending ? (
+          <Typography>Loading...</Typography>
+        ) : (
+          <ActivityDashboard
+            activities={activities}
+            selectedActivity={selectedActivity}
+            selectActivity={handleSelectActivity}
+            cancelSelectActivity={handleCancelSelectActivity}
+            editMode={editMode}
+            openForm={handleOpenForm}
+            closeForm={handleFormClose}
+            submitForm={handleSumbitForm}
+            deleteActivity={handleDelete}
+          />
+        )}
       </Container>
     </>
   );
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -11,12 +11,19 @@ import "@fontsource/outfit/700.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import theme from "./app/layout/theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme} defaultMode="light" noSsr>
-      <CssBaseline />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <CssBaseline />
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
 );
