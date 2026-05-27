e5f6983eda59c066f555923e16d34499853c5531
Elham-EN <ilhamrezaie@gmail.com>
2026-05-27 20:24:29 +1000

    made dashboard responsive for mobile screen

--- Client/src/features/activities/ActivityDashboard.tsx
+++ Client/src/features/activities/ActivityDashboard.tsx
@@ -6,12 +6,14 @@ import LoadingFallback from "../../lib/components/LoadingFallback";
 function ActivityDashboard(): React.ReactElement {
   return (
     <Grid container spacing={3}>
-      <Grid size={7}>
+      <Grid size={{ xs: 12, md: 5 }} sx={{ order: { xs: 1, md: 2 } }}>
+        Activity Filter
+      </Grid>
+      <Grid size={{ xs: 12, md: 7 }} sx={{ order: { xs: 2, md: 1 } }}>
         <Suspense fallback={<LoadingFallback message="Loading activities" />}>
           <ActivityList />
         </Suspense>
       </Grid>
-      <Grid size={5}>Activity Filter</Grid>
     </Grid>
   );
 }
