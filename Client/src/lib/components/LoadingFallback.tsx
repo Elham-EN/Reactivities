c88221a63839d6107f6e997ccf4df228fab5883a
Elham-EN <ilhamrezaie@gmail.com>
2026-05-27 13:46:57 +1000

    added special spinner gridloader for fallback

--- Client/src/lib/components/LoadingFallback.tsx
+++ Client/src/lib/components/LoadingFallback.tsx
@@ -1,21 +1,30 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
+import { GridLoader } from "react-spinners";
+import { useTheme } from "@mui/material/styles";
 
 interface Props {
   message: string;
 }
 
 function LoadingFallback({ message }: Props): React.ReactElement {
+  const theme = useTheme();
+
   return (
     <Box
       sx={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        gap: 3,
         minHeight: "100vh",
       }}
     >
-      <Typography variant="h4">{message}...</Typography>
+      <GridLoader color={theme.palette.primary.main} size={16} />
+      <Typography variant="h6" color="text.secondary">
+        {message}...
+      </Typography>
     </Box>
   );
 }
